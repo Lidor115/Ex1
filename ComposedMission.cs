220d900130f383dc6f2873ef02fe0f4f59e93431
lidor115 <lidor115@gmail.com>
2019-03-24 14:40:06 +0200

    coding style for all the program
    coding style for all the program

--- ComposedMission.cs
+++ ComposedMission.cs
@@ -6,28 +6,34 @@ using System.Threading.Tasks;
 
 namespace Excercise_1
 {
-    /*Composed mission*/
+    /*Composed mission class - inhirit IMission*/
     public class ComposedMission : IMission
     {
         private List<func> compList;
-        private string missionName; 
+        private string missionName;
+        /*Constructor of Composed mission - get a name and initialize list*/
         public ComposedMission(string name)
         {
             this.compList = new List<func>();
             this.missionName = name;
         }
-
+        /*add functions to the list of the missions*/
         public ComposedMission Add(func f)
         {
             compList.Add(f);
             return this; 
         }
+        /*initialize the name of the mission*/
         public string Name => this.missionName;
 
+        /*initialize the Type of the mission to Composed*/
         public string Type => "Composed";
 
+        /*EventHandler of IMission*/
         public event EventHandler<double> OnCalculate;
 
+        /*calculate the mission (use the functions and invoke the 
+         event handler about the soloution*/
         public double Calculate(double value)
         {
             double newVal = value;
--- SingleMission.cs
+++ SingleMission.cs
@@ -6,23 +6,25 @@ using System.Threading.Tasks;
 
 namespace Excercise_1
 {
+    /*Single mission class - inhirit IMission*/
     public class SingleMission : IMission
     {
         private func function;
         string missionName;
-       
+        /*Constructor of Single mission - get a name and function to do*/
         public SingleMission(func f, string mn)
         {
             this.function = f;
             this.missionName = mn;
         }
-
+        /*initialize the name of the mission*/
         public string Name => missionName;
-
+        /*initialize the Type of the mission to Single*/
         public string Type => "Single";
-
+        /*EventHandler of IMission*/
         public event EventHandler<double> OnCalculate;
-
+        /*calculate the mission (use the function and invoke the 
+         event handler about the soloution*/
         public double Calculate(double val)
         {
             double newval = this.function(val);
